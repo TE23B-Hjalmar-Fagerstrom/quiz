e381bb31140c54258c6aba769dd8eb3f3650560a
Hjalmar Fagerström <hjalmar.fagerstrom@elev.ga.ntig.se>
2024-09-10 13:21:36 +0200

    första

---
+++ quiz/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Vad heter du?");
+string name = Console.ReadLine();
+
+Console.WriteLine($"Hej {name} och välkomen till min fråge sport.");
+Console.WriteLine("");
+
+
+int rätt = 0;
+
+if (rätt == 4)
+{
+    Console.WriteLine($"Bra jobbat {name}! Du fick alla rätt!");
+}
+
+if (rätt == 2 )
+{
+    Console.WriteLine($"Bra försök {name}. Du fick {rätt} frågor rätt.");
+}
+
+if (rätt == 0 )
+{
+    Console.WriteLine($"Bättre lyka nästa gång {name}. Du fick {rätt} frågor rätt.");
+}
+
+Console.ReadLine();
